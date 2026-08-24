b8ef59140ce2300c1eff5952d735eb23e45e2e4b
alexandr-galkin <sangalkin8@gmail.com>
2026-08-24 21:16:12 +0300

    refactor: make theme manager use puzzle theme

--- src/themes/ThemeManager.js
+++ src/themes/ThemeManager.js
@@ -1,45 +1,35 @@
-import { LibraryTheme } from './library/LibraryTheme.js';
+import { PuzzleTheme } from './PuzzleTheme.js';
 
+/** Registry for runtime-switchable visual themes. */
 export class ThemeManager {
-  constructor() {
-    this.themes = {
-      library: new LibraryTheme(),
-    };
-    
+  constructor({ documentRef = document } = {}) {
+    this.themes = { library: new PuzzleTheme({ documentRef }) };
     this.currentThemeName = 'library';
     this.current = this.themes.library;
   }
 
-  getTheme(name) {
-    return this.themes[name] || this.current;
-  }
+  /** @param {string} name @returns {object} */
+  getTheme(name) { return this.themes[name] || this.current; }
 
+  /** Switch the active theme without reloading the page. @param {string} name @returns {boolean} */
   setTheme(name) {
-    if (this.themes[name]) {
-      this.current = this.themes[name];
-      this.currentThemeName = name;
-      return true;
-    }
-    return false;
+    if (!this.themes[name]) return false;
+    this.current = this.themes[name];
+    this.currentThemeName = name;
+    return true;
   }
 
-  getAllThemes() {
-    return Object.values(this.themes);
-  }
+  /** @returns {object[]} */
+  getAllThemes() { return Object.values(this.themes); }
+  /** @returns {string[]} */
+  getThemeNames() { return Object.keys(this.themes); }
+  /** @returns {string} */
+  getCurrentThemeName() { return this.currentThemeName; }
 
-  getThemeNames() {
-    return Object.keys(this.themes);
-  }
-
-  getCurrentThemeName() {
-    return this.currentThemeName;
-  }
-
+  /** @param {string} name @param {object} theme @returns {boolean} */
   registerTheme(name, theme) {
-    if (theme && typeof theme.renderBook === 'function') {
-      this.themes[name] = theme;
-      return true;
-    }
-    return false;
+    if (!theme || typeof theme.renderBook !== 'function') return false;
+    this.themes[name] = theme;
+    return true;
   }
 }
